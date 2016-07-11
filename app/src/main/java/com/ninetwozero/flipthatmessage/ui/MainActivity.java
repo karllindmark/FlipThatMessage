edbca3cc196f4d8513ec16c61a3ecd7ebb520281
Karl Lindmark <karl@ninetwozero.com>
2016-07-11 23:34:08 +0200

    Add export functionality

--- app/src/main/java/com/ninetwozero/flipthatmessage/ui/MainActivity.java
+++ app/src/main/java/com/ninetwozero/flipthatmessage/ui/MainActivity.java
@@ -1,9 +1,14 @@
 package com.ninetwozero.flipthatmessage.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.button_flip)
     public Button flipButton;
 
+    private ShareActionProvider shareActionProvider;
     private TextWatcher textWatcher = createTextWatcher();
 
     @Override
@@ -41,6 +47,20 @@ public class MainActivity extends AppCompatActivity {
         flipButton.setEnabled(input.length() > 0);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menu.findItem(R.id.action_share));
+        shareActionProvider.setShareIntent(createNewShareIntent(getString(R.string.tableflip)));
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
     private TextWatcher createTextWatcher() {
         return new TextWatcher() {
             @Override
@@ -55,8 +75,18 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
+                if (shareActionProvider != null) {
+                    shareActionProvider.setShareIntent(createNewShareIntent(editable.toString()));
+                }
                 flipButton.setEnabled(editable.length() > 0);
             }
         };
+    }
+
+    private Intent createNewShareIntent(final String text) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+        return shareIntent;
     }
 }
